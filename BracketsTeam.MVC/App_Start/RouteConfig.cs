a184e3ffaa520bedd92e4cb48cbcadcf6ac4b0fd
fire-frog <fabian.barrera.q@gmail.com>
2017-03-16 17:40:16 -0300

    Added js clipboard/bracket. Updated test Page

--- BracketsTeam.MVC/App_Start/RouteConfig.cs
+++ BracketsTeam.MVC/App_Start/RouteConfig.cs
@@ -18,6 +18,12 @@ namespace BracketsTeam.MVC
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
+
+            routes.MapRoute(
+                name: "Brackets",
+                url: "Brackets/",
+                defaults: new { controller = "Brackets", action = "Index", id = UrlParameter.Optional }
+            );
         }
     }
 }
